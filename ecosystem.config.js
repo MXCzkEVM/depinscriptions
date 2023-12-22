1ee179a9da45b128c3d68dc11c60eda9d2c2b636
Mr.Mao <951416545@qq.com>
2023-12-23 01:35:19 +0800

    chore: update pm2 args config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'wannsee-geoscriptions',
     script: 'npm',
-    args: 'run build-preview-server',
+    args: 'run start:prod',
     watch: ['.'],
     watch_delay: 1000,
     ignore_watch: ['node_modules'],
