c2ddb4b69dad9a04906d382a105152d1b310fb13
Mr.Mao <951416545@qq.com>
2023-12-26 13:03:16 +0800

    feat: update server logs

--- packages/server/src/services/tasks.service.ts
+++ packages/server/src/services/tasks.service.ts
@@ -31,6 +31,7 @@ export class TasksService {
     const lastBlockNumber = await this.provider.getLastBlockNumber()
     const startBlockNumber = await getIndexerLastBlock()
     const endBlockNumber = Math.min(startBlockNumber + 10, lastBlockNumber)
+    this.logger.log(`Blockchain current block number is ${lastBlockNumber}`)
     if (startBlockNumber >= endBlockNumber)
       return
     this.logger.log(`Regularly scan blockchain ${startBlockNumber} to ${endBlockNumber} blocks`)
