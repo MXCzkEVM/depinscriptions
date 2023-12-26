1bea469048e58f8b2b10bf97ad63d09ef9e22436
Mr.Mao <951416545@qq.com>
2023-12-26 15:25:31 +0800

    fix: genapi to api url error

--- packages/client/genapi.config.ts
+++ packages/client/genapi.config.ts
@@ -4,7 +4,7 @@ import source from './genapi.json'
 export default defineConfig({
   input: { json: source },
   pipeline: 'swag-fetch-ts',
-  baseURL: 'process.env.NEXT_SERVER_URL',
+  baseURL: 'process.env.NEXT_PUBLIC_SERVER_URL',
   output: {
     main: 'src/api/index.ts',
     type: 'src/api/index.type.ts',
