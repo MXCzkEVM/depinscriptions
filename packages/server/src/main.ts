090980efc0fe63911c3387d163846bd49b31028c
Mr.Mao <951416545@qq.com>
2023-12-28 16:17:00 +0800

    chore: update server swagger api json url

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,17 +4,20 @@ import { Logger } from '@nestjs/common'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const options = new DocumentBuilder()
+  const config = new DocumentBuilder()
     .setTitle('geoscriptions')
     .setDescription('geoscriptions api')
     .setVersion('1.0.0')
     .build()
 
   const app = await NestFactory.create(AppModule)
-  app.enableCors()
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('swagger', app, document)
+  const document = SwaggerModule.createDocument(app, config)
 
+  SwaggerModule.setup('swagger', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  })
+
+  app.enableCors()
   await app.listen(process.env.NEST_LISTEN_SERVER_PORT)
   new Logger('Nest').log(`The server has started and is listening on port ${process.env.NEST_LISTEN_SERVER_PORT}`)
 }
