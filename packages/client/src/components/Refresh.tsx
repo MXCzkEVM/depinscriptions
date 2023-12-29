261bfb5d0d5339aefc8bf1c9ec662aacb6ab4f37
Mr.Mao <951416545@qq.com>
2023-12-29 15:23:23 +0800

    chore: delete unused mitt hooks

--- packages/client/src/components/Refresh.tsx
+++ packages/client/src/components/Refresh.tsx
@@ -5,6 +5,7 @@ import { useEventBus } from '@/hooks'
 
 interface RefreshProps {
   event?: string
+  onClick?: any
 }
 
 function Refresh(props: RefreshProps) {
@@ -12,8 +13,8 @@ function Refresh(props: RefreshProps) {
   const { emit: reload } = useEventBus(props.event || 'reload:page')
 
   return (
-    <div className="flex gap-2 items-center cursor-pointer" onClick={reload}>
-      <span>{t('Refresh')}</span>
+    <div className="flex gap-2 items-center cursor-pointer" onClick={props.onClick || reload}>
+      <span className="hidden md:inline-block">{t('Refresh')}</span>
       <Icon size={18}>
         <IconRefresh />
       </Icon>
