e79a7795e78bc00e1faaaa978ce5a8e32bbcf17c
Mr.Mao <951416545@qq.com>
2024-01-07 17:26:20 +0800

    fix: production with proxy error

--- packages/inscription-service/src/main.ts
+++ packages/inscription-service/src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './app.module'
 import { withEthersHttpProxy, withNestjsCors, withNestjsListen, withNestjsRepairDecimal, withNestjsSwagger } from './bootstrap'
 
 async function bootstrap() {
-  const { NEST_LISTEN_SERVER_PORT } = process.env
+  const { NEST_LISTEN_SERVER_PORT, NODE_ENV } = process.env
   const app = await NestFactory.create(AppModule)
 
-  withEthersHttpProxy({ host: '127.0.0.1', port: 7890 })
+  console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
+
+  if (NODE_ENV === 'development')
+    withEthersHttpProxy({ host: '127.0.0.1', port: 7890 })
   withNestjsRepairDecimal(app)
   withNestjsSwagger(app)
   withNestjsCors(app)
