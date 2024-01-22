72bca7f2c43ef8cc3c9f96a0cadd4903bfadc75f
Mr.Mao <951416545@qq.com>
2024-01-22 11:31:41 +0800

    chore: change hexagon random

--- packages/inscription-frontend/src/utils/util.ts
+++ packages/inscription-frontend/src/utils/util.ts
@@ -1,32 +1,22 @@
 import { latLngToCell } from 'h3-js'
+import hexagons from './hexagons.json'
 
 export function noop(): any {}
 
 export async function getCurrentHexagon() {
-  let [latitude, longitude] = generateRandomCoordinates()
   try {
     const position = await new Promise<GeolocationPosition>((resolve, reject) => {
       navigator.geolocation.getCurrentPosition(resolve, reject)
     })
-    latitude = position.coords.latitude
-    longitude = position.coords.longitude
+    return latLngToCell(
+      position.coords.latitude,
+      position.coords.longitude,
+      7,
+    )
   }
-  catch (error) {}
-  return latLngToCell(
-    latitude,
-    longitude,
-    7,
-  )
-}
-
-export function generateRandomCoordinates() {
-  // 生成随机的经度（longitude）
-  const longitude = (Math.random() * 360) - 180
-  // 生成随机的纬度（latitude）
-  const latitude = (Math.random() * 180) - 90
-
-  // 返回经度和纬度的对象
-  return [longitude, latitude] as const
+  catch (error) {
+    return hexagons[Math.floor(Math.random() * hexagons.length)]
+  }
 }
 
 export function ejectBlankPage(url: string) {
@@ -52,7 +42,6 @@ export function waitForElement(selector: string, timeout = 3000): Promise<Elemen
         observer.disconnect()
       }
     })
-
     observer.observe(document.documentElement, { childList: true, subtree: true })
     setTimeout(reject, timeout)
   })
