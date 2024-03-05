37d0b5edfa7b0e454ba3853a95be9955348cbb61
Mr.Mao <951416545@qq.com>
2024-03-05 16:58:15 +0800

    chore: update code

--- packages/inscription-frontend/src/pages/index.tsx
+++ packages/inscription-frontend/src/pages/index.tsx
@@ -1,23 +1,13 @@
-/* eslint-disable no-alert */
 import type { ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Button } from '@mui/material'
 import { Layout } from '@/layout'
 import { DataGridScriptions, Refresh } from '@/components'
 
 function Page() {
   const { t } = useTranslation()
 
-  async function onConnect() {
-    const accounts = await window.ethereum?.request({
-      method: 'eth_requestAccounts',
-    })
-    alert(accounts)
-  }
-
   return (
     <>
-      <Button type="button" variant="contained" onClick={onConnect}>TEST</Button>
       <div className="my-first-step flex justify-between mt-[30px]">
         <span className="my-other-step text-xl sm:text-2xl text-primary font-bold">{t('Latest Scriptions')}</span>
         <Refresh />
